7e97fbdb602e71481c22d0ff5a3c02ecb6dd6642
Patato <pathonscript@gmail.com>
2022-11-15 19:15:57 +0700

    setup layout

---
+++ components/layout/main.tsx
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import React from "react";
+
+function Layout({ children }: React.PropsWithChildren) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/logo.svg" />
+
+        <meta name="twitter:title" content="Ohaiyo!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="author" content="Patato" />
+        <meta name="author" content="patato" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@Patatotota" />
+        <meta name="twitter:creator" content="@Patatotota" />
+        <meta
+          name="twitter:image"
+          content="https://class.patato.live/vaderfight.png"
+        />
+        <meta property="og:site_name" content="Ohaiyo!" />
+        <meta name="og:title" content="Ohaiyo!" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://class.patato.live/vaderfight.png"
+        />
+        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
+
+      {children}
+    </>
+  );
+}
+
+export default Layout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Chakra from "../components/chakra";
 import Fonts from "../components/fonts";
+import Layout from "../components/layout/main";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Chakra>
       <Fonts />
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </Chakra>
   );
 }
